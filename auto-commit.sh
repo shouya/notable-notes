dea68318048a5103c2112b64c8f221df79df2bb9
Shou Ya (auto) <shouya@users.noreply.github.com>
2019-02-26 11:44:22 +0800

    Auto commit Tue Feb 26 03:44:22 UTC 2019

--- auto-commit.sh
+++ auto-commit.sh
@@ -4,7 +4,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 set -e
 
-num_dirty="$(git status --porcelain 2>/dev/null| grep "^??" | wc -l)"
+num_dirty="$(git status --porcelain 2>/dev/null| grep -e "^??" -e '^.M' | wc -l)"
 push_pending="$(git diff --shortstat 'origin/master')"
 
 if [[ "$num_dirty" -eq 0 && -z "$push_pending" ]]; then
@@ -17,6 +17,7 @@ if [[ "$num_dirty" -ne 0 ]]; then
     --no-gpg-sign \
     --author "Shou Ya (auto) <shouya@users.noreply.github.com>" \
     -am "Auto commit `date -u`"
+  git pull --rebase
 fi
 
 git push origin master
